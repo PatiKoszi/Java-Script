afdd601778e2a2beba690cabe72449d2fa3e314c
PatiKoszi <patrykkosznik@gmail.com>
2018-02-27 13:48:59 +0100

    JS mathematicks voulumes, if, else if

--- JS.js
+++ JS.js
@@ -1,1 +1,16 @@
-alert("Hellow, World");
+var myage =22;
+
+if (myage > 30) {
+
+    document.write("You are over 30!");
+}
+
+else if (myage < 10) {
+
+    document.write("Your are old");
+}
+
+else {
+
+    document.write("YOU CAN DO EVERYTHIG");
+}
