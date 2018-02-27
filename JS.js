c2525df22704353bea43bc137638ffb486b346b2
PatiKoszi <patrykkosznik@gmail.com>
2018-02-27 22:08:21 +0100

    For Loop with a href

--- JS.js
+++ JS.js
@@ -1,4 +1,4 @@
-var age = 5;
+/*var age = 5;
 
 while (age < 10){
 
@@ -7,4 +7,22 @@ while (age < 10){
 
 }
 
-    document.write('You are over 10')
+    document.write('You are over 10') */
+
+
+/*below it is exacly the same but with the "for" loop
+for (age = 5; age < 10; age++) {
+
+    console.log("Your age is less then 10")
+}
+
+document.write("You are over 10") */
+
+var links = document.getElementsByTagName("a");
+
+for (i =1; i <= links.length; i++) {
+
+    console.log("this is link number" +i);
+}
+
+document.write("all links now looped");
