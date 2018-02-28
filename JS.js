119b5444bf7432b7d49a717f2440ebff1feecc92
PatiKoszi <patrykkosznik@gmail.com>
2018-02-28 01:05:21 +0100

    Math.round, floor, ceil, PI strin, number

--- JS.js
+++ JS.js
@@ -1,9 +1,14 @@
-function getAverage (a, b, c, d) {
+var a = 5;
+var b = 10;
 
-    var average = (a+b+c+d) / 4;
-    console.log(average);
-    return average;
-}
+console.log(a + b);
+console.log(typeof (a + b));
 
-var myResult = getAverage(4, 5, 8, 9);
-console.log("The average is + myResult");
+console.log(Math.round(7.7));
+console.log(Math.round(7.2));
+console.log(Math.floor(7.7));
+console.log(Math.floor(7.2));
+console.log(Math.ceil(7.7));
+console.log(Math.ceil(7.2));
+console.log(Math.max(7, 9, 12, 4));
+console.log(Math.PI);
