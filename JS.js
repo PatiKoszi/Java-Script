6aac709b2a2ab8c7ac007f30ae83a84df73e9a5b
PatiKoszi <patrykkosznik@gmail.com>
2018-02-28 01:25:29 +0100

    NaN

--- JS.js
+++ JS.js
@@ -1,14 +1,11 @@
-var a = 5;
+var a = 6;
 var b = 10;
 
-console.log(a + b);
-console.log(typeof (a + b));
+if (isNaN(a)){ //if is not a number (a-to wartość)
 
-console.log(Math.round(7.7));
-console.log(Math.round(7.2));
-console.log(Math.floor(7.7));
-console.log(Math.floor(7.2));
-console.log(Math.ceil(7.7));
-console.log(Math.ceil(7.2));
-console.log(Math.max(7, 9, 12, 4));
-console.log(Math.PI);
+    console.log("Please provide a number")
+
+} else {
+
+    console.log("It is a number " + (a * b))
+}
