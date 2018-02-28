fd38ed8f0b3553c505f689ef2d24837013bc861b
PatiKoszi <patrykkosznik@gmail.com>
2018-02-28 22:43:58 +0100

    String

--- JS.js
+++ JS.js
@@ -1,11 +1,56 @@
-var a = 6;
-var b = 10;
+var myString = 'I\'m a probabley "the best King Ninja" at the planet'
 
-if (isNaN(a)){ //if is not a number (a-to wartość)
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.indexOf("King"));
+console.log(myString.indexOf("Ninja"));// There is no Ninja in the sentence
+//and the result is -1
 
-    console.log("Please provide a number")
+if (myString.indexOf("Ninja") === -1){
+
+    console.log("The word is not part of the sentence")
 
 } else {
 
-    console.log("It is a number " + (a * b))
+    console.log("the word Ninja starts at position " +
+    myString.indexOf("Ninja"));
 }
+
+var string1 = "abc"
+var string2 = "def"
+
+console.log(string1 === string2);
+
+var string1 = "abc"
+var string2 = "abc"
+
+console.log(string1 === string2);
+
+var string1 = "abc"
+var string2 = "ABC"
+
+console.log(string1 === string2);
+
+var string1 = "abc"
+var string2 = "ABC"
+
+console.log(string1.toLowerCase() === string2.toLowerCase());
+
+var string1 = "abc"
+var string2 = "def"
+
+console.log(string1 < string2);//becouse "a" is before "d" in alfabet
+
+var string1 = "abc"
+var string2 = "Def"
+
+console.log(string1 < string2); //lower case is greater then upper case
+
+
+
+
+
+
+
+
+
