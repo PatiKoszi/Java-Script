1705c7a9b70bbcc8ac46fef8cd9e41f24f1b05f9
PatiKoszi <patrykkosznik@gmail.com>
2018-03-03 21:58:30 +0100

    Creating a new JS Object

--- JS.js
+++ JS.js
@@ -1,11 +1,29 @@
-var myString = new String();
-myString = "Hellow";
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hellow";
 
-console.log(myString);
-console.log(myString.length);
-console.log(myString.toLowerCase());
+console.log(myArray[0]);
+console.log(myArray[1]);
 
-var myString2 = "History of Poland";
-console.log(myString2.length);
-console.log(myString2.toUpperCase());
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+
+console.log(myCar.driver);
+console.log(myCar.maxSpeed);
+
+myCar.drive = function(){console.log("now driving");};
+
+myCar.drive();
+
+var myCar2 = {
+    maxSpeed: 50,
+    driver: 'Net Ninja',
+    drive: function (speed, time) {
+        console.log(speed * time);
+    }
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
 
